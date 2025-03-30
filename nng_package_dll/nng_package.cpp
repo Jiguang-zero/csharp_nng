187caa613c00533173ea12ae3270ab8b42c14460
Zearo Jiguang <zearo_jiguang1@163.com>
2025-03-30 22:38:55 +0800

    nng_package

--- nng_package_dll/nng_package.cpp
+++ nng_package_dll/nng_package.cpp
@@ -2,11 +2,93 @@
 
 #include <iostream>
 
-nng_socket * ConnectUrl(const std::string& url) {
+nng_socket * ConnectRequestSocket(const std::string& url) {
     auto * socket = new nng_socket();
+    // Request type.
     if (const int res = nng_req0_open(socket); res != 0) {
+        Release(socket);
+        return nullptr;
+    }
+
+    if (nng_dial(*socket, url.c_str(), nullptr, 0) != 0) {
         nng_socket_close(*socket);
+        Release(socket);
         return nullptr;
     }
     return socket;
+}
+
+int SetSocketSendTimeOut(const nng_socket *socket, const int milliseconds) {
+    return nng_socket_set_ms(*socket, NNG_OPT_SENDTIMEO, milliseconds);
+}
+
+int SetSocketReceiveTimeOut(const nng_socket *socket, const int milliseconds) {
+    return nng_socket_set_ms(*socket, NNG_OPT_RECVTIMEO, milliseconds);
+}
+
+int SetSocketReConnectMinTime(const nng_socket *socket, const int milliseconds) {
+    return nng_socket_set_int(*socket, NNG_OPT_RECONNMINT, milliseconds);
+}
+
+int SetSocketReConnectMaxTime(const nng_socket *socket, const int milliseconds) {
+    return nng_socket_set_int(*socket, NNG_OPT_RECONNMAXT, milliseconds);
+}
+
+void Release(const nng_socket *socket) {
+    nng_socket_close(*socket);
+    delete socket;
+}
+
+const char * GetErrorString(const int rv) {
+    // if rv is not a correct nng_err data, it will return nullptr.
+    if ((rv >= 0 && rv <= 31) || rv == 999 || rv == 1000 || rv == 0x10000000 || rv == 0x20000000) {
+        const auto rv_enum = static_cast<nng_err>(rv);
+        return nng_strerror(rv_enum);
+    }
+    return nullptr;
+}
+
+int Send(const nng_socket *socket, const char *data, const size_t size) {
+    nng_msg* msg = nullptr;
+
+    // ReSharper disable once CppDFAMemoryLeak
+    int rv = nng_msg_alloc(&msg, 0);
+
+    if (rv != 0) {
+        // ReSharper disable once CppDFAMemoryLeak
+        return rv;
+    }
+
+    rv = nng_msg_append(msg, data, size);
+    if (rv != 0) {
+        nng_msg_free(msg);
+        return rv;
+    }
+
+    rv = nng_sendmsg(*socket, msg, NNG_FLAG_NONBLOCK);
+    nng_msg_free(msg);
+
+    return rv;
+}
+
+const char * GetReceiveMessage(const nng_socket *socket, const char *request, const int request_size) {
+    nng_msg *msg = nullptr;
+
+    int rv = Send(socket, request, request_size);
+    if (rv != 0) {
+        return nullptr;
+    }
+
+    rv = nng_recvmsg(*socket, &msg, NNG_FLAG_NONBLOCK);
+    if (rv != 0) {
+        nng_msg_free(msg);
+        return nullptr;
+    }
+    const auto response_size = nng_msg_len(msg);
+    auto * response = new char[response_size + 1];
+    memcpy(response, nng_msg_body(msg), request_size);
+    response[request_size + 1] = '\0';
+
+    nng_msg_free(msg);
+    return response;
 }
--- nng_package_dll/nng_package.h
+++ nng_package_dll/nng_package.h
@@ -14,6 +14,69 @@
     #define NNG_API
 #endif
 
-extern "C" NNG_API nng_socket* ConnectUrl(const std::string& url);
+/**
+ * connect url
+ * @param url const std::string&
+ * @return Pointer Address. nng_socket*
+ */
+extern "C" NNG_API nng_socket* ConnectRequestSocket(const std::string& url);
+
+/**
+ * Set sending timeout.
+ * @param socket nng_socket*
+ * @param milliseconds
+ * @return return the result of setting sending timeout. 0 is success.
+ */
+extern "C" NNG_API int SetSocketSendTimeOut(const nng_socket* socket, int milliseconds);
+
+/**
+ * Set receiving timeout.
+ * @param socket nng_socket*
+ * @param milliseconds
+ * @return return the result of setting receive timeout. 0 is success
+ */
+extern "C" NNG_API int SetSocketReceiveTimeOut(const nng_socket* socket, int milliseconds);
+
+/**
+ * Set min reconnect time.
+ * @param socket
+ * @param milliseconds
+ * @return
+ */
+extern "C" NNG_API int SetSocketReConnectMinTime(const nng_socket* socket, int milliseconds);
+
+/**
+ * Set max reconnect time.
+ * @param socket
+ * @param milliseconds
+ * @return
+ */
+extern "C" NNG_API int SetSocketReConnectMaxTime(const nng_socket* socket, int milliseconds);
+
+/**
+/**
+ * Close Connect
+ * @param socket nng_socket*
+ */
+extern "C" NNG_API void Release(const nng_socket* socket);
+
+/**
+ * We will get the result state when we call connect, bind, receive, send, etc...
+ * The type of this state is integer. And we can call this function to see what does the state mean.
+ * @param rv
+ * @return const char*
+ */
+extern "C" NNG_API const char* GetErrorString(int rv);
+
+int Send(const nng_socket* socket, const char* data, size_t size);
+
+/**
+ *
+ * @param socket
+ * @param request
+ * @param request_size  The size of request
+ * @return
+ */
+extern "C" NNG_API const char* GetReceiveMessage(const nng_socket* socket, const char* request, int request_size);
 
 #endif //NNG_PACKAGE_DLL_LIBRARY_H
